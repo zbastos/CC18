f72e6f90496df368f4b9842b48984a4f2cca41d9
Eletrolize <bruno.ferreira.96@hotmail.com>
2018-03-14 12:30:19 +0000

    Bota remédio das internets, ou neste caso, dos UDPs

--- src/UDPMonitor.py
+++ src/UDPMonitor.py
@@ -1,23 +1,47 @@
 
-import time
+import time, socket, sys, struct
 
 
 class UDPMonitor:
     def __init__(self, table, m_time, ip, port):
         self.multicast_Time = m_time
         self.stateTable = table
-        self.multicast_IP = ip
-        self.multicast_PORT = port
+        self.multicast_group = (ip, port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def multicast(self):
         """
         Multicast function, will keep running as long as UDPMonitor is up
         """
+
+        self.socket.settimeout(1)
+        message = "OlHa, SoY Tu PaDrE"
+
         while True:
             time.sleep(self.multicast_Time)
-            #get table
-            for entry in self.stateTable.getInfo():
-                self.sendUDP(entry)
+            # depende das infos da tabela, ir buscar o maior (?)
+            ttl = struct.pack('b',1)
+            self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+            try:
+                print ('sending %s') %message
+                sent = self.socket.sendto(message, self.multicast_group)
+
+                while True:
+                    print ('Waiting to receive')
+
+                    try:
+                        data, server = self.socket.recvfrom(16) #(?)
+                    except socket.timeout:
+                        print >>sys.stderr, 'Timed out, no response'
+                        break
+                    else:
+                        print('Received message %s from %s') % (data, server)
+
+            finally:
+                print("Closing socket") #(?)
+
+        self.socket.close()
 
     def sendUDP(self, entry):
         """
